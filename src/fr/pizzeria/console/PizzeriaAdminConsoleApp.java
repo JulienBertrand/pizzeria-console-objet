f5d950e66020528ce5b08b73d371194dd794c1b8
Julien Bertrand <julien.bertrand6384@gmail.com>
2017-07-28 18:20:33 +0200

    update du programme pizza

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -2,6 +2,8 @@ package fr.pizzeria.console;
 
 import java.util.Scanner;
 
+import fr.pizzeria.model.Pizza;
+
 /**
  * 
  * 
@@ -17,11 +19,21 @@ public class PizzeriaAdminConsoleApp {
 		Scanner questionUser = new Scanner(System.in);
 		int choix = 0;
 		
+		
+		Pizza[] pizzas = new Pizza[100];
+		pizzas[0] = new Pizza ("PEP", "pépéroni", 12.50);
+		pizzas[1] = new Pizza ("MAR", "Margherita", 14.00);
+		pizzas[2] = new Pizza ("REI", "La Reine", 11.50);
+		pizzas[3] = new Pizza ("FRO", "La 4 fromages", 12.00);
+		pizzas[4] = new Pizza ("CAN", "La cannibale", 12.50);
+		pizzas[5] = new Pizza ("SAV", "La savoyarde", 13.00);
+		pizzas[6] = new Pizza ("ORI", "L'orientale", 13.50);
+		
 		do {
 			System.out.println("***** Pizzeria Administration *****");
 			System.out.println("1. Lister les pizzas");
 			System.out.println("2. Ajouter une nouvelle pizza");
-			System.out.println("3. Mettre � jour une pizza");
+			System.out.println("3. Mettre à jour une pizza");
 			System.out.println("4. Supprimer une pizza");
 			System.out.println("99. Sortir");
 			System.out.println("Veuillez faire un choix s'il vous plait");
@@ -31,11 +43,27 @@ public class PizzeriaAdminConsoleApp {
 			if (choix == 1) {
 
 				System.out.println("Liste des pizzas");
+				
+				for (int i = 0; i < pizzas.length; i++) {
+					if (pizzas[i] !=null) {
+					System.out.println(pizzas[i].getCode()+ ">" + pizzas[i].getNom() + ">" + pizzas[i].getPrix() + "u20AC");
+					}
+				}
+					
 				System.out.println();
+				System.out.println("**************************************");
 			}
 			if (choix == 2) {
+			//ajouter nouvelle pizza
+				//je demande à l'utlilsateur le code de la nouvelle pizza
+				//je demande à l'utlilsateur le nom de la nouvelle pizza
+				//je demande à l'utlilsateur le prix de la nouvelle pizza
+				
+				//je créé la nouvelle pizza
+				
+				// je recherche un emplacement libre dans le tableau pour y insérer la nouvelle pizza
 
-				System.out.println("Mise � jour d�une pizza");
+				System.out.println("Mise à jour d'une pizza");
 				System.out.println();
 			}
 			if (choix == 3) {
@@ -45,7 +73,7 @@ public class PizzeriaAdminConsoleApp {
 			}
 			if (choix == 4) {
 
-				System.out.println("Suppression d�une pizza");
+				System.out.println("Suppression d'une pizza");
 				System.out.println();
 			}
 			if (choix == 99) {
@@ -56,5 +84,6 @@ public class PizzeriaAdminConsoleApp {
 		} while (choix != 99);
 		
 		 questionUser.close();
+		 
 	}
 }
--- src/fr/pizzeria/model/Pizza.java
+++ src/fr/pizzeria/model/Pizza.java
@@ -1,17 +1,41 @@
 package fr.pizzeria.model;
 
 public class Pizza {
+	private static int nextId = 0;
+	private int id;
+	private String code; 
+	private String nom;
+	private double prix;
 	
-	int id;
-	String code; 
-	String nom;
-	double prix;
-	
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
 	public Pizza(String code, String nom, double prix) {
-		super();
-		this.id = id;
+		this.id = nextId;
 		this.code = code;
 		this.nom = nom;
 		this.prix = prix;
+		nextId++;
 	}
 }
