7862880bc173a7a17f3d3bd85fcb7bb60becac96
Julien Bertrand <julien.bertrand6384@gmail.com>
2017-07-30 22:24:39 +0200

    ajout de quelques pistes pour le choix 2

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -18,17 +18,16 @@ public class PizzeriaAdminConsoleApp {
 
 		Scanner questionUser = new Scanner(System.in);
 		int choix = 0;
-		
-		
+
 		Pizza[] pizzas = new Pizza[100];
-		pizzas[0] = new Pizza ("PEP", "pépéroni", 12.50);
-		pizzas[1] = new Pizza ("MAR", "Margherita", 14.00);
-		pizzas[2] = new Pizza ("REI", "La Reine", 11.50);
-		pizzas[3] = new Pizza ("FRO", "La 4 fromages", 12.00);
-		pizzas[4] = new Pizza ("CAN", "La cannibale", 12.50);
-		pizzas[5] = new Pizza ("SAV", "La savoyarde", 13.00);
-		pizzas[6] = new Pizza ("ORI", "L'orientale", 13.50);
-		
+		pizzas[0] = new Pizza("PEP", "Pépéroni", 12.50);
+		pizzas[1] = new Pizza("MAR", "Margherita", 14.00);
+		pizzas[2] = new Pizza("REI", "La Reine", 11.50);
+		pizzas[3] = new Pizza("FRO", "La 4 fromages", 12.00);
+		pizzas[4] = new Pizza("CAN", "La cannibale", 12.50);
+		pizzas[5] = new Pizza("SAV", "La savoyarde", 13.00);
+		pizzas[6] = new Pizza("ORI", "L'orientale", 13.50);
+
 		do {
 			System.out.println("***** Pizzeria Administration *****");
 			System.out.println("1. Lister les pizzas");
@@ -43,32 +42,60 @@ public class PizzeriaAdminConsoleApp {
 			if (choix == 1) {
 
 				System.out.println("Liste des pizzas");
-				
+				System.out.println();
+
 				for (int i = 0; i < pizzas.length; i++) {
-					if (pizzas[i] !=null) {
-					System.out.println(pizzas[i].getCode()+ ">" + pizzas[i].getNom() + ">" + pizzas[i].getPrix() + "u20AC");
+					if (pizzas[i] != null) {
+						System.out.println(
+								pizzas[i].getCode() + ">" + pizzas[i].getNom() + ">" + pizzas[i].getPrix() + " \u20AC");
 					}
 				}
-					
+
 				System.out.println();
 				System.out.println("**************************************");
 			}
 			if (choix == 2) {
-			//ajouter nouvelle pizza
-				//je demande à l'utlilsateur le code de la nouvelle pizza
-				//je demande à l'utlilsateur le nom de la nouvelle pizza
-				//je demande à l'utlilsateur le prix de la nouvelle pizza
-				
-				//je créé la nouvelle pizza
-				
-				// je recherche un emplacement libre dans le tableau pour y insérer la nouvelle pizza
 
-				System.out.println("Mise à jour d'une pizza");
+				System.out.println("Ajouter une nouvelle pizza");
 				System.out.println();
+
+				String nouveauNom = null;
+				String nouveauCode = null;
+				double nouveauPrix = 0;
+
+				System.out.println("Veuillez saisir le code"); // je demande à l'utlilsateur le code de la nouvelle pizza
+
+					
+					nouveauCode = questionUser.toString();
+				
+
+				System.out.println("Veuillez saisir le nom"); // je demande à l'utlilsateur le nom de la nouvelle pizza
+				
+				
+					nouveauNom = questionUser.toString();
+				
+				
+
+				System.out.println("Veuillez saisir le prix"); // je demande à l'utlilsateur le prix de la nouvelle pizza
+				nouveauPrix = questionUser.nextInt();
+
+				Pizza nouvellePizza = new Pizza(nouveauCode, nouveauNom, nouveauPrix); // je créé la nouvelle pizza
+
+				
+				
+				
+
+				// ajouter nouvelle pizza
+
+				
+
+				// je recherche un emplacement libre dans le tableau pour y insérer la nouvelle
+				// pizza
+
 			}
 			if (choix == 3) {
 
-				System.out.println("Ajout d'une nouvelle pizza");
+				System.out.println("Modifier un pizza");
 				System.out.println();
 			}
 			if (choix == 4) {
@@ -82,8 +109,8 @@ public class PizzeriaAdminConsoleApp {
 			}
 
 		} while (choix != 99);
-		
-		 questionUser.close();
-		 
+
+		questionUser.close();
+
 	}
 }
